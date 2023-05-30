f07f09f0d5aa6d2143c2a30e4effb03063e624c8
alirezayoom <alirezayoom@gmail.com>
2023-05-30 22:49:55 +0430

    add text and dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("BMI calculator"),
-        ),
+void main() => runApp(BMIcalculator());
+
+class BMIcalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xFF0A0E21),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
       ),
-    ),
-  );
+      home: InputPage(),
+    );
+  }
 }
